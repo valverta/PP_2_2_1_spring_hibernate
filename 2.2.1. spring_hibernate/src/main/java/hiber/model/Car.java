a1e05b4e74377129830c4d03233053e08742ea55
borya <boris98@gmail.com>
2023-05-05 01:00:37 +0500

    Spring_Hibernate complete

---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -0,0 +1,25 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "series")
+    private int series;
+    @Column(name = "model")
+    private String model;
+    public Car() {
+
+    }
+    public Car(String model) {
+        this.model = model;
+    }
+
+    @Override
+    public String toString() {
+        return model;
+    }
+}
